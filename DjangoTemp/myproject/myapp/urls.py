cfdea4953540f7be9251b9a56b626661b5f4c575
123Lex123 <orlovskiy713@gmail.com>
2024-09-17 22:13:32 +0300

    Django auth

--- DjangoTemp/myproject/myapp/urls.py
+++ DjangoTemp/myproject/myapp/urls.py
@@ -1,8 +1,12 @@
+
 from django.urls import path
-from .views import index
-from .views import poll
+from .views import index, poll, register
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', index, name='index'),
-    path('poll/<int:question_id>/', poll, name='poll')
+    path('poll/<int:question_id>/', poll, name='poll'),
+    path('login/', auth_views.LoginView.as_view(template_name='myapp/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='index'), name='logout'),
+    path('register/', register, name='register'),
 ]
--- DjangoTemp/myproject/myapp/views.py
+++ DjangoTemp/myproject/myapp/views.py
@@ -1,9 +1,9 @@
-from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse
-from .models import Question, Choice
-from .forms import PollForm
+from .models import Choice
 from .models import Question
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
@@ -14,6 +14,11 @@ def index(request):
 def poll(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     choices = Choice.objects.filter(question=question)
+
+    if not request.user.is_authenticated:
+        return render(request, 'myapp/not_authenticated.html', {
+            'message': "Авторизуйтесь, чтобы пройти опрос."
+        })
 
     if request.method == 'POST':
         selected_choice_id = request.POST.get('option')
@@ -29,3 +34,15 @@ def poll(request, question_id):
             'options': choices
         }
     })
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'myapp/register.html', {'form': form})
